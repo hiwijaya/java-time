33335b0748c5eaacb0d79e22936172f8c4d9303f
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-08-16 11:34:47 +0700

    added DayOfWeek test

---
+++ src/test/java/com/hiwijaya/time/DayOfWeekTest.java
@@ -0,0 +1,25 @@
+package com.hiwijaya.time;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+
+/**
+ * @author Happy Indra Wijaya
+ */
+public class DayOfWeekTest {
+
+    @Test
+    void create() {
+
+        DayOfWeek dayOfWeek1 = DayOfWeek.SUNDAY;
+        DayOfWeek dayOfWeek2 = dayOfWeek1.plus(7);
+        DayOfWeek dayOfWeek3 = dayOfWeek1.plus(1);
+
+        System.out.println(dayOfWeek1);
+        System.out.println(dayOfWeek2);
+        System.out.println(dayOfWeek3);
+
+    }
+
+}
